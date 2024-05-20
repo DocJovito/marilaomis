172ada0fc356854e34f9e0785d29985247358c98
DocJovito <105496716+DocJovito@users.noreply.github.com>
2024-05-20 23:27:47 +0800

    aaaa

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -29,14 +29,11 @@
             <li class="nav-item">
               <RouterLink to="/programs/view" active-class="active-link" class="nav-link">Programs</RouterLink>
             </li>
-            <!-- Add Log In link -->
-            <li v-if="!isLoggedIn" class="nav-item">
-              <RouterLink to="/login" active-class="active-link" class="nav-link">Log In</RouterLink>
-            </li>
           </ul>
           <!-- Right-aligned navbar links -->
           <ul class="navbar-nav ms-auto">
-            <li v-if="userId === '111'" class="nav-item">
+            <li class="nav-link">Welcome User {{ userId }}</li>
+            <li v-if="userId < 1" class="nav-item">
               <RouterLink class="nav-link" active-class="active-link" to="/login">Log In</RouterLink>
             </li>
             <li v-else class="nav-item">
@@ -55,10 +52,8 @@ import { useStore } from 'vuex';
 
 //jovy
 const store = useStore();
-// const userId = store.state.userState[0].userID;
-// const userLevel = store.state.userState[0].userLevel;
-const userId = localStorage.getItem('userid');
-const userLevel = localStorage.getItem('usertype');
+const userId = store.state.userState[0].userID;
+const userLevel = store.state.userState[0].userLevel;
 const userName = localStorage.getItem('name');
 
 // Check if user is logged in
