0440dbb7dfb6bfc5c6a35b16da979bfa0322fa9a
DocJovito <105496716+DocJovito@users.noreply.github.com>
2024-07-11 23:08:26 +0800

    mod print 0.07

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -24,7 +24,7 @@
               <div class="form-group text-center">
                 <button type="submit" class="btn btn-primary btn-block">Log In</button>
               </div>
-              <p>Version 0.06</p>
+              <p>Version 0.07</p>
             </form>
 
             <form v-if="otpSent" @submit.prevent="verifyOtp">
